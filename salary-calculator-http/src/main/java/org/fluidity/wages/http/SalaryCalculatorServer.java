085dd18cbc5c75e726127a4749011a86c43b0be1
Tibor Adam Varga <tibor.adam.varga@gmail.com>
2016-08-14 20:59:34 +0300

    Cosmetics.

--- salary-calculator-http/src/main/java/org/fluidity/wages/http/SalaryCalculatorServer.java
+++ salary-calculator-http/src/main/java/org/fluidity/wages/http/SalaryCalculatorServer.java
@@ -19,28 +19,50 @@ final class SalaryCalculatorServer implements Application {
 
     static final int DEFAULT_PORT = 8080;
 
-    private final ContainerTermination stopping;
     private final Log log;
-
     private final Vertx vertx;
-
     private final ResourceHandler resources;
     private final SalaryCalculatorHandler calculator;
 
     SalaryCalculatorServer(final VertxInstance vertx,
-                           final SalaryCalculatorHandler calculator,
                            final ResourceHandler resources,
+                           final SalaryCalculatorHandler calculator,
                            final ContainerTermination stopping,
                            final Log<SalaryCalculatorServer> log) {
         this.vertx = vertx.get();
         this.resources = resources;
         this.calculator = calculator;
-        this.stopping = stopping;
         this.log = log;
+
+        stopping.add(() -> {
+            this.vertx.close();
+            log.info("Server stopped");
+        });
     }
 
     public void run(final String... arguments) {
-        final HttpServer server = vertx.createHttpServer();
+        final String host = "localhost";        // hard-coded for now, as this is only a demo
+        final int port;
+
+        try {
+            port = arguments.length > 0 ? Integer.parseInt(arguments[0]) : DEFAULT_PORT;
+        } catch (final NumberFormatException error) {
+            throw new IllegalArgumentException(String.format("Expected a number: %s", arguments[0]));
+        }
+
+        final Router router = router(vertx);
+        final HttpServer server = server(vertx);
+
+        server.requestHandler(router::accept).listen(port, host);
+
+        log.info("Server listening on http://%s:%d", host, port);
+    }
+
+    private HttpServer server(final Vertx vertx) {
+        return vertx.createHttpServer();
+    }
+
+    private Router router(final Vertx vertx) {
         final Router router = Router.router(vertx);
 
         // Computes salary from an uploaded CSV file.
@@ -57,19 +79,14 @@ final class SalaryCalculatorServer implements Application {
             response.end();
         });
 
+        return router;
+    }
+
+    private int integerArgument(final String[] arguments, final int index, final int defaultValue) {
         try {
-            final String host = "localhost";        // hard-coded for now, as this is only a demo
-            final int port = arguments.length > 0 ? Integer.parseInt(arguments[0]) : DEFAULT_PORT;
-
-            server.requestHandler(router::accept).listen(port, host);
-
-            log.info("Server listening on http://%s:%d", host, port);
-            stopping.add(() -> {
-                vertx.close();
-                log.info("Server stopped");
-            });
+            return arguments.length > index ? Integer.parseInt(arguments[index]) : defaultValue;
         } catch (final NumberFormatException error) {
-            log.error("supplied port '%s' is not a number", arguments[0]);
+            throw new IllegalArgumentException(String.format("Expected a number: %s", arguments[index]));
         }
     }
 }
